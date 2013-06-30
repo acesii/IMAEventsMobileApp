81232051118b06b6c5a34da830d80a5f889f3ec3
Ian Ibbotson <ian.ibbotson@k-int.com>
2013-06-30 18:42:41 +0100

    Bit more work...

--- www/js/index.js
+++ www/js/index.js
@@ -22,13 +22,19 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
+
+        // Open a database connection and store it at the app level
         this.db = window.openDatabase("Database", "1.0", "PhoneGap Demo", 200000);
+
+        // Create a new transaction, and invoke the "updateEventsData" method defined under "app"
         this.db.transaction(this.updateEventsData,
-                            function() {
+                            function(err) {
                               // Error Callback
+                              alert("Error "+err);
                             },
                             function() {
                               // Success Callback
+                              alert("OK");
                             });
     },
     // Bind Event Listeners
@@ -57,13 +63,34 @@ var app = {
         console.log('Received Event: ' + id);
     },
     updateEventsData: function(tx) {
+
+      var highest_timestamp_seen = 0;
+
       // Get or create our status table - this will record the highest timestamp we have seen so far
-      tx.executeSql('CREATE TABLE IF NOT EXISTS WS_FETCH_CURSOR ( id unique, highest_ts )');
+      tx.executeSql('CREATE TABLE IF NOT EXISTS WS_FETCH_CURSOR ( id unique, name, highest_ts )');
+
       // Get or create our events table - list of events
       tx.executeSql('CREATE TABLE IF NOT EXISTS EVENT ( id unique, name, event_date, address )');
 
+      // Having made sure all the tables exist, see if we have any cursor entries for tracking where we are up
+      // to in downloading event data.
+      tx.executeSql('SELECT * FROM WS_FETCH_CURSOR where name = "events"', [], 
+                    function(tx, results) {
+                      if ( results.rows.length == 0 ) {
+                        // No matching rows - First time we have run this routine - Set up the tracking row
+                        tx.executeSql('INSERT INTO WS_FETCH_CURSOR(name, highest_ts) VALUES ("events",0)');
+                      }
+                      else {
+                        // Run previously - Extract the max timestamp and use it
+                        highest_timestamp_seen = results.rows.item(0).highest_ts
+                      }
+                    }, 
+                    function(err) {
+                      alert("Problem getting highest timestamp : "+err);
+                    });
+
       // Now fetch the latest data
-      $.getJSON ("http://localhost:8080/IMAEventsServer/UpcomingEvents/list/Sheffield?addedSince=0",
+      $.getJSON ("http://localhost:8080/IMAEventsServer/UpcomingEvents/list/Sheffield?addedSince="+highest_timestamp_seen,
         function (data) {
           for (index = 0; index < data.events.length; ++index) {
           }
